9075d42a35a3968ba50f27af1ca59536b6711b91
Nick Pope <pope410211@gmail.com>
2015-05-13 22:39:18 -0400

    attemtp to complete pt1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 (function (window) {
 	'use strict';
-
+debugger;
 	// Your starting point. Enjoy the ride! for commit
+	var elements = document.querySelectorAll('ul');
+
+	_.forEach(elements, function(element, index, elements){
+    element.addEventListener('click', function(){
+        console.log(elements, index, element);
+        element.parentElement.classList.toggle('completed');
+    });
+});
+
+
 
 })(window);
