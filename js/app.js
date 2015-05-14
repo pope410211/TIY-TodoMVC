21141434d402628f69fa5b76f3971f3cbba96c0f
Nick Pope <pope410211@gmail.com>
2015-05-14 16:16:05 -0400

    comment edit

--- js/app.js
+++ js/app.js
@@ -27,12 +27,12 @@
 				var task = document.querySelector('input.new-todo').value;
 				todos.taskList.push(task);
 						document.querySelector('input.new-todo').value= "";
-				
+
 					console.log(todos.taskList);
 
 					// THEN:
 					// DONE  Save the thing to remember (task) to the list of things to remember(taskList)
-					// TODO  Remove the things to remember(task) from the "what needs to be done?"box(input new todo)
+					// DONE  Remove the things to remember(task) from the "what needs to be done?"box(input new todo)
 					// TODO  Update the number of tasks in the footer...
 					// TODO  Add a new task(ul.todo-list > li) to the list of tasks in the display (ul.todo-list)
 				}
